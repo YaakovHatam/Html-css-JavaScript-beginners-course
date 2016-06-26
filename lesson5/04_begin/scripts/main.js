8e44ed08c308a8319a0f9b67c86222dfd94d1f6e
kob-e <kobe@MBK.local>
2016-06-26 21:29:46 +0300

    lot of new lessons

---
+++ lesson5/04_begin/scripts/main.js
@@ -0,0 +1,30 @@
+/*
+    לקלוט 2 משתנים על ידי שימוש בפונקציה 
+    prompt
+    לאחר מכן לשאול את המשתמש האם הוא רוצה לעשות חיבור (לחיצה על אישור) או חיסור לחיצה על ביטול
+    להשתמש ב
+    confirm
+    לאחר מכן להקפיץ את התשובה על ידי שימוש ב
+    alert
+
+
+
+*/
+
+
+var res = promt("Please enter a number");
+//var {name} = {value}
+var res1 = confirm("Plus or minus");
+
+var res2 = alert(parseInt(res););
+
+if (true) {
+    
+}
+else {
+}
+
+//CAML case
+var firstValueEver;
+
+
---
+++ test/scripts/main.js
@@ -0,0 +1,30 @@
+
+//alert(2/3);
+
+//console.log("Logging to browser console");
+
+//console.warn("Cannot be negetive value");
+
+//console.error("Cannot divide by 0");
+
+"i am a string"
+
+10
+
+true
+
+
+var StringVariable = "i am a string";
+
+var NumberVariable = 10;
+
+var BooleanVariable = true;
+
+typeof someVariable
+
+
+alert(typeof "str");
+alert(typeof 3);
+
+var VariableName = "value";
+
